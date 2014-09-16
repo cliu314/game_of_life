c10de877a871c72279bf32add3e6d50eb9d796e1
cliu314 <clareliu@mit.edu>
2014-09-16 16:56:35 -0400

    updated readme file and removed unnecessary files

--- qunit-example/tests_proj1.js
+++ qunit-example/tests_proj1.js
@@ -1,4 +1,5 @@
 
+// test that the correct boundaries are being calculated when finding neighbors for cells on the left edge of the grid
 test("testing left edge", function() {
 	var canvas = new Canvas('cnv', 5);
 	var grid = new Grid(5, canvas);
@@ -7,11 +8,20 @@ test("testing left edge", function() {
 	grid.createLiveCell(0,2);
 	grid.createLiveCell(0,0);
 
+
+	// find live neighbors for each live cell
 	grid.getAllNeighbors();
 	var n = grid.grid[0][1].neighbors;
 	equal(n, 2);
+
+	var n = grid.grid[0][0].neighbors;
+	equal(n, 1);
+
+	var n = grid.grid[0][2].neighbors;
+	equal(n, 1);
 });
 
+// test that game applies update rules correctly
 test("testing left edge update", function() {
 	var canvas = new Canvas('cnv', 3);
 	var grid = new Grid(3, canvas);
@@ -45,6 +55,7 @@ test("testing left edge update", function() {
 	equal(grid.grid[2][2].live, 0);
 });
 
+// test that the correct boundaries are being calculated when finding neighbors for cells on the right edge
 test("testing right edge", function() {
 	var canvas = new Canvas('cnv', 5);
 	var grid = new Grid(5, canvas);
@@ -91,6 +102,7 @@ test("testing right edge update", function() {
 	equal(grid.grid[2][2].live, 0);
 });
 
+// test that the correct boundaries are being calculated when finding neighbors for cells on the top edge
 test("testing top edge", function() {
 	var canvas = new Canvas('cnv', 5);
 	var grid = new Grid(5, canvas);
@@ -136,6 +148,7 @@ test("testing top edge update", function() {
 	equal(grid.grid[2][2].live, 0);
 });
 
+// test that the correct boundaries are being calculated when finding neighbors for cells on the bottom edge
 test("testing bottom edge", function() {
 	var canvas = new Canvas('cnv', 5);
 	var grid = new Grid(5, canvas);
@@ -179,6 +192,7 @@ test("testing bottom edge update", function() {
 });
 
 
+// test behavior of a simple oscillator
 test("testing blinker", function() {
 	var canvas = new Canvas('cnv', 3);
 	var grid = new Grid(3, canvas);
@@ -205,5 +219,15 @@ test("testing blinker", function() {
 	equal(grid.grid[1][2].live, 0);
 	equal(grid.grid[2][0].live, 0);
 	equal(grid.grid[2][1].live, 1);
+	equal(grid.grid[2][2].live, 0);
+	grid.evolve();
+	equal(grid.grid[0][0].live, 0);
+	equal(grid.grid[0][1].live, 0);
+	equal(grid.grid[0][2].live, 0);
+	equal(grid.grid[1][0].live, 1);
+	equal(grid.grid[1][1].live, 1);
+	equal(grid.grid[1][2].live, 1);
+	equal(grid.grid[2][0].live, 0);
+	equal(grid.grid[2][1].live, 0);
 	equal(grid.grid[2][2].live, 0);
 });
